45421c3df4f1eef0d40d98b463a70f775d1fc246
tanukidevelop <tanukidevelop453145@gmail.com>
2021-06-15 07:24:39 +0900

    - feature デッキ一覧をカードで表示するように変更

--- poceka_deck_manager/lib/main.dart
+++ poceka_deck_manager/lib/main.dart
@@ -6,7 +6,6 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   List<DeckRecipeModel> _deckList = [
     DeckRecipeModel(
         deckName: "1", deckCode: "11", deckMemo: "111", createDate: "1111"),
@@ -16,45 +15,11 @@ class MyApp extends StatelessWidget {
         deckName: "3", deckCode: "33", deckMemo: "333", createDate: "3333")
   ];
 
-  Widget MyListView() {
-    return ListView.separated(
-        itemBuilder: (BuildContext context, int index) {
-          return Container(
-              padding: EdgeInsets.all(10),
-              child: Row(
-                children: <Widget>[
-                  Container(
-                    alignment: Alignment.topLeft,
-                    child: Text(_deckList[index].deckName.toString(),
-                        style: TextStyle(fontSize: 25)),
-                  ),
-                  Spacer(),
-                  Container(
-                    alignment: Alignment.bottomRight,
-                    child: Text(_deckList[index].deckCode.toString(),
-                        style: TextStyle(fontSize: 20)),
-                  )
-                ],
-              ));
-        },
-        separatorBuilder: (BuildContext context, int index) => Divider(),
-        itemCount: _deckList.length);
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'デッキレシピ一覧'),
@@ -74,16 +39,6 @@ class DeckRecipeModel {
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -95,23 +50,12 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
         centerTitle: true, // 中央寄せを解除
@@ -123,8 +67,6 @@ class _MyHomePageState extends State<MyHomePage> {
             // _count++;
           }),
         ),
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
         actions: <Widget>[
           IconButton(
@@ -142,37 +84,29 @@ class _MyHomePageState extends State<MyHomePage> {
         ],
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'ポケモンが表示されている行を長押しすると編集ができます',
+              'デッキレシピが表示されている行を長押しすると編集ができます',
             ),
 
             Flexible(
               //Flexibleでラップ
               child: ListView.builder(
-                  itemCount: 100,
-                  itemBuilder: (BuildContext context, int index) {
-                    return index % 2 == 0 ? Text('item no: $index') : Divider();
-                  }),
+                itemBuilder: (BuildContext context, int index) {
+                  return Card(
+                    child: Padding(
+                      child: Text(
+                        'デッキ名：$index',
+                        style: TextStyle(fontSize: 22.0),
+                      ),
+                      padding: EdgeInsets.all(20.0),
+                    ),
+                  );
+                },
+                itemCount: 10,
+              ),
             ),
 
             // Text(
@@ -186,7 +120,7 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
