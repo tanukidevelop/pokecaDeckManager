01f3e0e00f4f7af29a2134a9da4ce2fa5b45853e
tanukidevelop <tanukidevelop453145@gmail.com>
2021-06-16 00:29:07 +0900

    - 新規デッキレシピ作成画面で新規追加選択時に配列に追加されるように記述

--- pokeca_deck_manager/lib/add.dart
+++ pokeca_deck_manager/lib/add.dart
@@ -3,6 +3,9 @@ import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 import 'main.dart';
 
 class AddPage extends StatelessWidget {
+  List<DeckRecipeModel> _deckList;
+  AddPage(this._deckList);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -73,7 +76,15 @@ class AddPage extends StatelessWidget {
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(10),
                   ),
-                  onPressed: () {},
+                  onPressed: () {
+                    final DeckRecipeModel deckModel = DeckRecipeModel(
+                        deckName: "ハイクラスデッキ インテレオンVmaxデッキ",
+                        deckCode: "xYcxcx-bj7co1-8D8c8G",
+                        deckMemo:
+                            "ハイクラスデッキ ゲンガーVmaxデッキハイクラスデッキ ゲンガーVmaxデッキハイクラスデッキ ゲンガーVmaxデッキハイクラスデッキ ゲンガーVmaxデッキ",
+                        createDate: "2021年6月15日");
+                    _deckList.add(deckModel);
+                  },
                 ),
                 Spacer(
                   flex: 5,
